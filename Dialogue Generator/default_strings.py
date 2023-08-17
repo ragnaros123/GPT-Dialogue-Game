703ea1fbabb5e743d1e8d9991f777a929f7d1d33
ragnaros123 <68516384+ragnaros123@users.noreply.github.com>
2023-08-17 20:17:18 +0100

    3

---
+++ Dialogue Generator/default_strings.py
@@ -0,0 +1,68 @@
+system_message = """
+You are a screenwriter, your job is to assist me in writing the dialogue for a detective game. 
+"""
+character_profiles = """
+ Character name: Oliver White (Detective)
+Role: A skilled and observant detective hired to investigate the case onboard.
+Want: Hailing from a humble background, Oliver is driven by a sense of justice and the thrill of untangling complex cases. His goal is to uncover the truth and bring the murderer to justice.
+Weakness: Oliver can sometimes overlook minor details, and his relentlessness can lead him to challenge powerful individuals, causing potential backlash.
+Personality: Oliver is analytical, persistent, and calm. He's methodical in his approach, patient when gathering evidence, and respectful but firm during interrogations.
+
+Character name: Thomas Jenkins (Merchant)
+Role: The murdered merchant, who was the father of Alexander and Jonathan, and the husband of Mrs. Thompson.
+Want: Before his death, Henry wanted to maintain control over his business and family. He was strict and expected a high standard of conduct from everyone around him.
+Weakness: Henry's ruthless business practices and domineering nature might have created many enemies who would wish him harm.
+Personality: Henry was authoritative, strict, and often abrasive. He held great sway over his family and employees.
+
+Character name: Mrs. Jenkins (Merchant's Wife)
+Role: The wife of the murdered merchant and involved in a secret affair.
+Want: Mrs. Thompson, trapped in a loveless marriage, sought solace in her affair with Phillip. Now, she wants to protect herself and Phillip from being implicated in her husband's murder.
+Weakness: Her illicit affair with Phillip makes her a potential suspect and compromises her credibility.
+Personality: Mrs. Thompson is secretive, cautious, and protective of her personal life. She is often defensive when under scrutiny.
+
+
+Character name: Phillip Anderson (Assistant)
+Role: The assistant of the murdered merchant, a potential suspect in the murder.
+Want: Phillip, a loyal servant of the merchant, is ambitious and craves the wealth and power his employer enjoyed. His goal is to protect his status and future, possibly even taking over the merchant's position.
+Weakness: Phillip can be easily provoked, and his anger may lead him to say more than he intends. His illicit relationship with the merchant's wife is a liability.
+Personality: Phillip is cunning, defensive, and quick to anger. He's deeply committed to preserving his position and will react strongly when accused.
+
+Character name: Jonathan Jenkins (Merchant's Younger Son)
+Role: The younger son of the murdered merchant, a person of interest in the murder.
+Want: Jonathan is the black sheep of the family. His relationship with his father, the merchant, was strained, and he desires to escape his family's shadow to create his own identity.
+Weakness: Jonathan's rebellious nature and poor relationship with his family make him a suspicious figure, potentially drawing unnecessary attention to himself.
+Personality: Jonathan is rebellious, independent, and often displays his emotions openly.
+
+Character name: Arthur Jenkins (Merchant's Older Son)
+Role: The responsible older son of the murdered merchant.
+Want: Alexander, who is more in line with the family's expectations, wants to protect his family's reputation, ensure justice for his father's murder, and possibly inherit the family business.
+Weakness: Alexander's devotion to his family may cause him to overlook details and potential suspects within the family circle.
+Personality: Alexander is loyal, protective, and takes his responsibilities seriously. He can be stern and authoritative when needed.
+
+Character name: Marvin Sullivan (Chef)
+Role: The ship's chef, suspected of murdering the merchant and the steward.
+Want: Marvin, a skilled chef, desires recognition for his culinary prowess. Accusations of murder jeopardize his reputation, and he wants to clear his name.
+Weakness: Marvin's passion for his work might make him vulnerable to manipulation. His job also puts him in a convenient position to be framed for the crime.
+Personality: Marvin is hardworking, dedicated, and proud of his work. He can be defensive when his craft is criticized.
+
+Character name: Beatrice Thompson (Maid)
+Role: The maid serving the merchant's family onboard.
+Want: Nancy wants to keep her position in the merchant's household and avoid getting involved in the murder investigation. However, her goal is also to tell the truth about what she saw.
+Weakness: Nancy's subservient position might make her overlook or undervalue crucial information she comes across during her work.
+Personality: Nancy is humble, observant, and dedicated to her duties. She tends to keep a low profile but is honest when questioned.
+
+Character name: Samuel Bennett (Captain)
+Role: The captain of the ship and the person who called in the detective.
+Want: Captain Roberts wants to maintain order on his ship, ensure the safety of his passengers, and solve the murder before the ship reaches the shore.
+Weakness: The captain's authoritative position and his rush to solve the case may make him overlook crucial evidence or hastily accuse the wrong people.
+Personality: Captain Roberts is authoritative, methodical, and responsible. He has a strong sense of duty and command over his crew.
+
+"""
+background_settings = """
+
+In the 1900s, aboard the SS Majestic, a wealthy merchant's assistant has an affair with the merchant's wife and conspires to poison him. The assistant complains about the food to 
+the captain, who tells the maid to  call the chef. The assistant seizes the opportunity and poisons the food while the chef is away. The wife provides a false alibi for the assistant.
+ The assistant attempts to poison the merchant again but is caught by a steward. They argue, and the assistant throws the steward off the deck. The steward's death is believed to be 
+ an accidental fall due to a storm.
+
+"""
